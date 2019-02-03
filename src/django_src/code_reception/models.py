112eda7a0f671bbac15bed7bf652dec3f63dfc61
Sergey Gorodetsky <runyoucl3v3rboy@gmail.com>
2019-02-04 00:39:45 +0300

    Added dummy database data, got ORM working

--- src/django_src/code_reception/models.py
+++ src/django_src/code_reception/models.py
@@ -1,12 +1,36 @@
+import json
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
 
+# tasks =[
+#     {
+#         'Question_id': '42',
+#         'Title': 'Sorting',
+#         'Text': 'this is a task',
+#         'Timeout': '3',
+#         'status': 'fail',
+#         'status_color': 'red',
+#         'test_success': ['True', 'True', 'False']
+#     }]
+
+#class TestSuccessField(models.CharField):
+
+
 class Task(models.Model):
+    Question_id = models.IntegerField(default=-1)
     title = models.CharField(max_length=100)
     text = models.TextField()
-    timeout = models.FloatField()
-    solve_status = models.BooleanField()
+    timeout = models.FloatField(default=3.0)
+    solve_status = models.CharField(max_length=20, default='no run')
+    status_color = models.CharField(max_length=30, default='red')
+    tests_success = models.TextField(max_length=100, default=json.dumps([]))
+    user_set = models.ManyToManyField(User)
 
+    def __str__(self):
+        return self.title
 
+# class Student(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     tasks = models.ManyToManyField(Task)
--- src/django_src/code_reception/views.py
+++ src/django_src/code_reception/views.py
@@ -1,6 +1,10 @@
+import json
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
+from .models import Task
+
 
 # Create your views here.
 SUPPORTED_TOOLSETS = [
@@ -8,43 +12,54 @@ SUPPORTED_TOOLSETS = [
     {'name': 'c++14', 'cmd': 'g++ main.cpp -Wall -fpermissive -std=c++14'},
 ]
 
-tasks =[
-    {
-        'Question_id': '42',
-        'Title': 'Sorting',
-        'Text': 'this is a task',
-        'Timeout': '3',
-        'status': 'fail',
-        'status_color': 'red',
-        'test_success': ['True', 'True', 'False']
-    },
-    {
-        'Question_id': '43',
-        'Title': 'Lookup',
-        'Text': 'this is a task 2',
-        'Timeout': '4',
-        'status': 'success',
-        'status_color': 'green',
-        'test_success': ['True', 'False', 'False']
-
-    },
-    {
-        'Question_id': '43',
-        'Title': 'Binary trees',
-        'Text': 'Ipsum Lorem',
-        'Timeout': '4',
-        'status': 'no run',
-        'status_color': 'black',
-        'test_success': []
-
-    }
-
-]
+# tasks =[
+#     {
+#         'Question_id': '42',
+#         'Title': 'Sorting',
+#         'Text': 'this is a task',
+#         'Timeout': '3',
+#         'status': 'fail',
+#         'status_color': 'red',
+#         'test_success': ['True', 'True', 'False']
+#     },
+#     {
+#         'Question_id': '43',
+#         'Title': 'Lookup',
+#         'Text': 'this is a task 2',
+#         'Timeout': '4',
+#         'status': 'success',
+#         'status_color': 'green',
+#         'test_success': ['True', 'False', 'False']
+#
+#     },
+#     {
+#         'Question_id': '43',
+#         'Title': 'Binary trees',
+#         'Text': 'Ipsum Lorem',
+#         'Timeout': '4',
+#         'status': 'no run',
+#         'status_color': 'black',
+#         'test_success': []
+#
+#     }
+#
+# ]
 
 def code_view(request):
+
+    #import pdb;
+    # tasks = list(Task.objects.all())
+    #pdb.set_trace()
+    # print(tasks)
     context = {
-        'tasks': tasks
+        #'tasks': tasks
+        'tasks': list(Task.objects.all())
     }
+    #context['tasks']['test_success'] = json.loads(context['tasks']['test_success'])
+    for e in context['tasks']:
+        print(json.loads(e.tests_success))
+        e.tests_success = json.loads(e.tests_success)  # hack, fix later
+    #pdb.set_trace()
     return render(request, 'code_reception/code.html', context)
 
 
