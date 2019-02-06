28a4d7c6e1a2a5418e49bee5c40de3f7c109dc3f
Sergey Gorodetsky <runyoucl3v3rboy@gmail.com>
2019-02-06 23:46:57 +0300

    added registration

--- src/django_src/django_src/urls.py
+++ src/django_src/django_src/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import sys
+sys.path.append("..")
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('code_reception.urls')),
+    path('register/', user_view.register, name='register'),
     #path('', include('code_reception.urls')),
 ]
